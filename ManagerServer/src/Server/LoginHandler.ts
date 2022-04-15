e29f451415c5f1a46cc2268a68a9008f590926fb
Flavio Garrido <ing.flaviogarrido@gmail.com>
2022-04-15 19:38:54 -0300

    Section 4 - 30 Refactoring with Typescript Abstract classes

--- ManagerServer/src/Server/LoginHandler.ts
+++ ManagerServer/src/Server/LoginHandler.ts
@@ -1,19 +1,16 @@
-import { on } from "events";
 import { IncomingMessage, ServerResponse } from "http";
 import { HTTP_CODES, HTTP_METHODS } from "../Sahred/Model";
-import { Account, Handler, TokenGenerator } from "./Model";
+import { BaseRequestHandler } from "./BaseRequestHandler";
+import { Account,  TokenGenerator } from "./Model";
 
-export class LoginHandler implements Handler{
+export class LoginHandler extends BaseRequestHandler{
 
-    private req: IncomingMessage;
-    private res: ServerResponse;
+    
     private tokenGenerator: TokenGenerator;
 
     public constructor(req:IncomingMessage, res:ServerResponse, tokenGenerator: TokenGenerator){
-        this.req = req;
-        this.res = res;
+        super(req,res)
         this.tokenGenerator = tokenGenerator;
-    
     }
 
     public async handlerRequest():Promise<void>{
@@ -28,13 +25,9 @@ export class LoginHandler implements Handler{
 
     }
 
-    private async handleNotFound() {
-        this.res.statusCode = HTTP_CODES.NOT_FOUND;
-        this.res.write('not found');        
-    }
     private async handlePost(){
         try{
-            const body = await this.getRequestBody(); 
+            const body: Account = await this.getRequestBody(); 
             const sessionToken = await this.tokenGenerator.generateToken(body);
             if (sessionToken){
                 this.res.statusCode = HTTP_CODES.CREATED;
@@ -49,24 +42,7 @@ export class LoginHandler implements Handler{
         }
     }
 
-    private async getRequestBody(): Promise<Account>{
-        return new Promise((resolve, reject)=>{
-            let body = '';
-            this.req.on('data', (data:string)=>{
-                body += data
-            }) 
-            this.req.on('end', ()=> {
-                try{
-                    resolve(JSON.parse(body));
-                }catch(error){
-                    reject(error);
-                }
-            });
-            this.req.on('error',(error:any)=>{
-                reject(error);
-            })
-        });
-    }
+   
 
 
 
--- ManagerServer/src/Server/UserHandler.ts
+++ ManagerServer/src/Server/UserHandler.ts
@@ -1,19 +1,15 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { HTTP_CODES, HTTP_METHODS } from "../Sahred/Model";
 import { UserDBA } from "../User/UsersDBA";
-import { Handler } from "./Model";
+import { BaseRequestHandler } from "./BaseRequestHandler"; 
 import { Utils } from "./Utils";
 
-export class UserHandler implements Handler{
+export class UserHandler extends BaseRequestHandler{
 
-    private req: IncomingMessage;
-    private res: ServerResponse;
     private userDBA: UserDBA = new UserDBA();
 
-    public constructor(req:IncomingMessage, res:ServerResponse){
-        this.req = req;
-        this.res = res; 
-    
+    public constructor(req:IncomingMessage, res: ServerResponse){
+        super(req,res)
     }
 
     public async handlerRequest(): Promise<void>{
@@ -26,13 +22,7 @@ export class UserHandler implements Handler{
                 break;
         }
     }
-    
-
-    private async handleNotFound() {
-        this.res.statusCode = HTTP_CODES.NOT_FOUND;
-        this.res.write('not found');        
-    }
-
+     
     private async handlerGet(){
         const parseUrl = Utils.getUrlParameters(this.req.url);
         const a = '';
