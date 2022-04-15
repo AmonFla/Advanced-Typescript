fdaa0567d1606f73e735cf3e0a858b0bfa6c3b21
Flavio Garrido <ing.flaviogarrido@gmail.com>
2022-04-15 09:14:58 -0300

    Section 3 - 16 Login handler

---
+++ ManagerServer/src/Server/LoginHandler.ts
@@ -0,0 +1,20 @@
+import { IncomingMessage, ServerResponse } from "http";
+
+export class LoginHandler{
+
+    private req: IncomingMessage;
+    private res: ServerResponse;
+
+    public constructor(req:IncomingMessage, res:ServerResponse){
+        this.req = req;
+        this.res = res;
+    
+    }
+
+    public handlerRequest():void{
+        
+
+    }
+
+
+}
