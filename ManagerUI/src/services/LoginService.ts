87ad93f88b4cbedb299f24da32c2151b06113932
Flavio Garrido <ing.flaviogarrido@gmail.com>
2022-04-16 17:25:15 -0300

    Section 6 - 47 Http call from the browser

--- ManagerUI/src/services/LoginService.ts
+++ ManagerUI/src/services/LoginService.ts
@@ -1,14 +1,28 @@
 import { SessionToken } from "../models/AuthenticationModel"
 
+const baseUrl = 'http://localhost:8080/';
+const loginUrl = baseUrl+'login';
+
 export class LoginService{
 
     public async login(username:string, password: string): Promise<SessionToken | undefined>{
-        if(username == 'user' && password == '123'){
-            return {
-                username: "some user"
-            } as any
-        }else{
-            return undefined
+        let options = {
+            method: 'POST',
+            headers:{
+               'Content-Yype': 'application/json'
+            },
+            body: JSON.stringify({
+               username,
+               password
+            });
         }
+
+        const result = await fetch(loginUrl, options);
+        if(result.status == 201){
+            return await result.json();
+        }else{
+            return undefined;
+        }
+
     }
 }
