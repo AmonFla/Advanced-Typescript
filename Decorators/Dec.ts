94ceae3beb026fff3c31025642e62ee5ec8e774c
Flavio Garrido <ing.flaviogarrido@gmail.com>
2022-04-18 15:27:28 -0300

    Section 7 - Decorators introduction

---
+++ Decorators/Dec.ts
@@ -0,0 +1,65 @@
+
+
+const someObject = {
+    someProperty: 'inicial'
+}
+
+
+class Manager{
+
+   // @watchChange
+    @linkValue(someObject)
+    someProperty: string
+}
+
+// watchChange(Manager.prototype, 'someProperty')
+
+const manager: Manager = new Manager();
+manager.someProperty = '123';
+console.log(someObject.someProperty)
+manager.someProperty = '456'
+console.log(someObject.someProperty);
+
+
+function linkValue(otherObject:any){
+    return function(target: any, key:string){
+        let property = target[key];
+
+        const getter = () => {
+            return property
+        }
+
+        const setter = (newVal: any) => { 
+            property = newVal;
+            otherObject[key] = newVal;
+        };
+
+        Object.defineProperty(target, key, {
+            get: getter,
+            set: setter,
+            configurable: true,
+            enumerable: true
+        })
+    }
+}
+
+function  watchChange(target: Object | any, key:string){
+
+    let property = target[key];
+
+    const getter = () => {
+        return property
+    }
+
+    const setter = (newVal: any) => {
+        console.log(`${key} changed from ${property} to ${newVal}`)
+        property = newVal;
+    };
+
+    Object.defineProperty(target, key, {
+        get: getter,
+        set: setter,
+        configurable: true,
+        enumerable: true
+    })
+}
